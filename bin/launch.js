af8d73a9f442de2cfc81c94d09aebb5fd01ed8ba
John Kristian <jmkristian@gmail.com>
2018-08-18 16:17:27 -0700

    Experiment with Express web server.

--- bin/launch.js
+++ bin/launch.js
@@ -12,33 +12,90 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 */
+const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan');
 const path = require('path');
 
 const ENCODING = 'utf-8';
 const PackItForms = 'pack-it-forms';
+const PackItMsgs = path.join(PackItForms, 'msgs');
+const PortFileName = path.join('bin', 'port.txt');
 
 switch(process.argv[2]) {
+case 'serve':
+    serve(getEnvironment(process.argv));
+    break;
 case 'new':
 case 'draft':
 case 'ready':
 case 'sent':
 case 'unread':
 case 'read':
-    openMessage();
+    console.log(expandForm(getEnvironment(process.argv)));
     break;
 default:
     console.log(process.argv[1] + ': unknown verb "' + process.argv[2] + '"');
 }
 
-function openMessage() {
-    var environment = {message_status: process.argv[2]};
-    for (var i = 3; i + 1 < process.argv.length; i = i + 2) {
-        environment[process.argv[i]] = process.argv[i+1];
+function getEnvironment(args) {
+    var environment = {message_status: args[2]};
+    for (var i = 3; i + 1 < args.length; i = i + 2) {
+        environment[args[i]] = args[i+1];
     }
     if (environment.msgno == '-1') { // a sentinel value
         delete environment.msgno;
     }
+    if (environment.MSG_FILENAME) {
+        var msgFileName = path.join(PackItMsgs, environment.MSG_FILENAME);
+        environment.message = fs.readFileSync(msgFileName, ENCODING);
+        if (!environment.msgno && isMyDraftMessage(environment.message_status)) {
+            // The MsgNo field is set by the sender. For a draft message, the sender is me.
+            // So pass it to the form as environment.message, shown as "My Message Number".
+            var found = /[\r\n]\s*MsgNo:\s*\[([^\]]*)\]/.exec(environment.message);
+            if (found) {
+                environment.msgno = found[1];
+            }
+        }
+        if (!environment.filename) {
+            found = /[\r\n]# *FORMFILENAME:([^\r\n]*)[\r\n]/.exec(environment.message);
+            if (found) {
+                environment.filename = found[1].trim();
+            }
+        }
+    }
+    var formFileName = path.resolve('pack-it-forms', environment.filename);
+    if (!fs.existsSync(formFileName)) {
+        throw new Error('no form file ' + formFileName);
+    }
+    return environment;
+}
+
+function serve(environment) {
+    const app = express();
+    app.set('etag', false); // convenient for debugging
+    app.use(morgan('dev'));
+    app.get('/form', function(req, res, next) {
+        res.set({'Content-Type': 'text/html; charset=' + ENCODING});
+        res.send(getForm(environment));
+    });
+    app.get('/msgs/:file', function(req, res, next) {
+        if (req.params.file != environment.msgno) {
+            res.sendStatus(400); // may not read other messages
+        } else {
+            res.set({'Content-Type': 'text/plain; charset=' + ENCODING});
+            res.send(environment.message ? environment.message : '');
+            // The client will quietly ignore an empty message.
+        }
+    });
+    app.get(/^\/.*/, express.static(PackItForms));
+    const server = app.listen(3000); // TODO: port 0, to get a temporary port
+    const address = server.address();
+    fs.writeFileSync(PortFileName, address.port + '', {encoding: ENCODING}); // advertise my port
+    console.log('Listening for HTTP requests on port ' + address.port + '...');
+}
+
+function getForm(environment) {
     console.log(environment);
     if (!environment.filename) {
         throw new Error('form file name is ' + environment.filename);
@@ -49,7 +106,7 @@ function openMessage() {
     }
     var form = fs.readFileSync(path.join(PackItForms, environment.filename), ENCODING);
     form = expandDataIncludes(form, JSON.stringify(environment));
-console.log(form);
+    return form;
 }
 
 /* Expand data-include-html elements, for example:
@@ -95,6 +152,7 @@ function expandDataInclude(data, query_objectJSON) {
         }
         if (init) {
             result += `<script type="text/javascript">
+  var formDefaultValues;
   if (!formDefaultValues) {
       formDefaultValues = [];
   }
